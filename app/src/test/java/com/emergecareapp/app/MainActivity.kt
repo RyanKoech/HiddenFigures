85da2bcabf35fd8d6b645068fcc8a75b5af30143
Lynette7 <lynettewanjiru7@gmail.com>
2023-04-05 19:02:10 +0300

    Emergecare app

---
+++ app/src/test/java/com/emergecareapp/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.emergecareapp.app
+
+import com.emergecareapp.app.appcomponents.base.BaseActivity
+import com.emergecareapp.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
